6e5b1894facd28c04d3ca162b92f5e60b0e13ad2
unknown <fagustinasanchez@gmail.com>
2019-08-05 00:15:18 -0300

    12.15

--- productos.php
+++ productos.php
@@ -20,14 +20,18 @@
                <section class="listado_productos">
                     <article class="producto">
                        <div class="img_producto">
-                           <img src="images/abcdario.jpeg" alt="ABC">
+                           <img src="images/abcdario.jpg" alt="ABC">
                        </div>
-                       <div class="btn_producto">
+                        <div class="txt-producto">
+                           <span class="categoria"> Icons </span>
+                           <span class="cantycolores"> 5 CUBOS - 3 COLORES </span>
+                           <div class="cardproductotexto">
+                               <p> Set de 5 cubos de maderas con 3 caras pintadas al agua. 100% hipoalergenico.</p>
+                                <span> $300.-</span>
+                           </div>
+                       </div>
+                           <div class="btn btn-dark">
                            <a href="producto.php">Personalizar</a>
-                       </div>
-                       <div class="txt-producto">
-                           <p>Contenido Articulo</p>
-                           <p class="precio"> $200</p>
                        </div>
                     </article>
 
