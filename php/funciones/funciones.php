673fdc402837498a61d64b983d6319b2cc38a0ce
juan <programascamacho@gmail.com>
2019-08-06 17:51:17 -0300

    prueba2

--- php/funciones/funciones.php
+++ php/funciones/funciones.php
@@ -1,5 +1,39 @@
 <?php
-$email= $_POST['email'];
-$nombre = $_POST['nombre'];
-$password = $_POST['password'];
- ?>
+$error = '';
+$verificacion = '';
+$contraseña = '';
+if (isset($_POST['submit'])) {
+    $nombre = $_POST['nombre'];
+    $email= $_POST['email'];
+    $password = $_POST['password'];
+    if (!empty($nombre)) {
+        $nombre = trim($nombre);
+        $nombre = htmlspecialchars($nombre);
+        $nombre = stripslashes($nombre);
+        $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+    }
+    else {
+        $error .= 'por favor agrega un nombre <br />';
+    }
+    if (!empty($email)) {
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        if (!filter_var($email, FILTER_SANITIZE_EMAIL)) {
+            $error .= 'Por favor ingresa un correo valido <br />';
+        }
+        else {
+            $verificacion = $email;
+        }
+    }
+    else {
+        $error .= 'Por favor ingresa  un email';
+    }
+    if (!empty($password)) {
+        if (strlen($password) > 3) {
+            $contraseña = "contraseña cumple los requisitos";
+        }
+        else {
+        $error .= "introduzca alguna contraseña valida";
+        }
+    }
+}
+?>
--- php/mod/ingreso.php
+++ php/mod/ingreso.php
@@ -3,24 +3,24 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
            <h2>LOG IN</h2>
-           <form class="contenedor_ap" action="index.php" method="post">
+           <form class="contenedor_ap" action="php/funciones/funciones.php" method="post">
                 <div class="input_contenedor_ap threed">
                     <i class="fas fa-envelope icon"></i>
-                    <input type="text" placeholder="Mail" required>
+                    <input type="text" placeholder="Email" name="email" required>
                 </div>
                 <div class="input_contenedor_ap threed">
                     <i class="fas fa-key icon"></i>
-                    <input type="password" placeholder="Contraseña" required>
+                    <input type="password" placeholder="Contraseña" name="password" required>
                 </div>
                 <div class="divformu" id="olvidastecontra">
                     <span>Olvidaste tu contrasena?</span>
                 </div>
                 <div class="divformu" id="recuerdame">
                     <label>
-                        <input type="checkbox" checked="checked" name="recuerdo"> Recuerdame!
+                        <input type="checkbox" checked="checked" name="recuerdame"> Recuerdame!
                     </label>
                 </div>
-                    <button class="btn btn-dark" type="submit" name="button" value="s" data-toggle="modal" data-target="#loginModalok">Ingresa</button>
+                    <button class="btn btn-dark" type="submit" name="submit"  data-toggle="modal" data-target="#loginModalok">Ingresa</button>
 
             </form>
         </div>
