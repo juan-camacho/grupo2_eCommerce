675a8105f5da860a10805e15f57b6b39063da73b
Juan Leonardo Camacho Rojas <juan.camacho@synthesis.com.ar>
2019-08-08 17:16:10 -0300

    validacion blanqueo

--- doc/usuariook.php
+++
@@ -1,9 +0,0 @@
-
-<div class="modal dialog" tabindex="-1" role="dialog" id="loginModalok">
-
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-        <?php echo $error ?>
-        </div>
-    </div>
-</div>
--- php/funciones/usuarios/nuevousuario.php
+++
@@ -1,6 +0,0 @@
-<?php
-$nombre = $_POST["nombre"];
-$password = $_POST["password"];
-$email = $_POST["email"];
-print_r($_POST);
- ?>
