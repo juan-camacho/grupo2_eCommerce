2cdc73fb540fa753cbe5189a03be6f6a779a1712
Pugigugi26 <fagustinasanchez@gmail.com>
2019-08-20 17:22:12 -0300

    Agus 20-08

---
+++ productos2.php
@@ -0,0 +1,255 @@
+<?php $tittle = "Tienda Babuino - productos"?>
+<!DOCTYPE html>
+<html lang="es">
+<?php require_once("php/mod/head.php")?>
+  <body>
+    <?php require_once("php/mod/header.php"); ?>
+              <main>
+                      <!-- FULLSLIDER -->
+                <!-- FILTROS -->
+                <div class="container-fluid" id="containerproducto">
+                  <form>
+                    <div class="form-group row">
+                      <label for="inputEmail3" class="col-12 col-md-4 col-lg-2">
+                          <h4> Potencia </h4>
+                          <h5> Lorem ipsum lorem ipsum </h5>
+                        </label>
+                        <div class="col-12 col-md-8 col-lg-10">
+                          <div class="form-check">
+                            <div class="btn">
+                              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                              <label class="form-check-label" for="gridRadios1">
+                                <div class="card">
+                                <img src="images/fedromini32.jpg" class="card-img-top" alt="...">
+                                <table class="table table-striped">
+
+                                <tbody>
+                                  <tr>
+                                    <div class="subtitulocards">
+                                      <h5>FEDRO 3.2W </h5>
+                                    </div>
+                                  <tr>
+                                    <th scope="row">Potencia</th>
+                                    <td>Mark</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Dimensión</th>
+                                    <td>Jacob</td>
+
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Flujo </th>
+                                    <td>Larry</td>
+                                  </tr>
+                                </tbody>
+                                </table>
+                              </div>
+
+                              </label>
+                            </div>
+                            <div class="btn">
+                              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                              <label class="form-check-label" for="gridRadios1">
+                                <div class="card">
+                                <img src="images/fedromini7.jpg" class="card-img-top" alt="...">
+                                <table class="table table-striped">
+
+                                <tbody>
+                                  <tr>
+                                    <div class="subtitulocards">
+                                      <h5>FEDRO 3.2W </h5>
+                                    </div>
+                                  <tr>
+                                    <th scope="row">Potencia</th>
+                                    <td>Mark</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Dimensión</th>
+                                    <td>Jacob</td>
+
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Flujo </th>
+                                    <td>Larry</td>
+                                  </tr>
+                                </tbody>
+                                </table>
+                              </div>
+
+                              </label>
+                            </div>
+
+                            <div class="btn">
+                              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                              <label class="form-check-label" for="gridRadios1">
+                                <div class="card">
+                                <img src="images/fedrosmall7.jpg" class="card-img-top" alt="...">
+                                <table class="table table-striped">
+
+                                <tbody>
+                                  <tr>
+                                    <div class="subtitulocards">
+                                      <h5>FEDRO 3.2W </h5>
+                                    </div>
+                                  <tr>
+                                    <th scope="row">Potencia</th>
+                                    <td>Mark</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Dimensión</th>
+                                    <td>Jacob</td>
+
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Flujo </th>
+                                    <td>Larry</td>
+                                  </tr>
+                                </tbody>
+                                </table>
+                              </div>
+
+                              </label>
+                            </div>
+                            <div class="btn">
+                              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                              <label class="form-check-label" for="gridRadios1">
+                                <div class="card">
+                                <img src="images/fedrosmall13.jpg" class="card-img-top" alt="...">
+                                <table class="table table-striped">
+
+                                <tbody>
+                                  <tr>
+                                    <div class="subtitulocards">
+                                      <h5>FEDRO 3.2W </h5>
+                                    </div>
+                                  <tr>
+                                    <th scope="row">Potencia</th>
+                                    <td>Mark</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Dimensión</th>
+                                    <td>Jacob</td>
+
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Flujo </th>
+                                    <td>Larry</td>
+                                  </tr>
+                                </tbody>
+                                </table>
+                              </div>
+
+                              </label>
+                            </div>
+
+                            <div class="btn">
+                              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                              <label class="form-check-label" for="gridRadios1">
+                                <div class="card">
+                                <img src="images/fedro18.jpg" class="card-img-top" alt="...">
+                                <table class="table table-striped">
+
+                                <tbody>
+                                  <tr>
+                                    <div class="subtitulocards">
+                                      <h5>FEDRO 3.2W </h5>
+                                    </div>
+                                  <tr>
+                                    <th scope="row">Potencia</th>
+                                    <td>Mark</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Dimensión</th>
+                                    <td>Jacob</td>
+
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Flujo </th>
+                                    <td>Larry</td>
+                                  </tr>
+                                </tbody>
+                                </table>
+                              </div>
+
+                              </label>
+                            </div>
+                            <div class="btn">
+                              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                              <label class="form-check-label" for="gridRadios1">
+                                <div class="card">
+                                <img src="images/fedro334.jpg" class="card-img-top" alt="...">
+                                <table class="table table-striped">
+
+                                <tbody>
+                                  <tr>
+                                    <div class="subtitulocards">
+                                      <h5>FEDRO 3.2W </h5>
+                                    </div>
+                                  <tr>
+                                    <th scope="row">Potencia</th>
+                                    <td>Mark</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Dimensión</th>
+                                    <td>Jacob</td>
+
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Flujo </th>
+                                    <td>Larry</td>
+                                  </tr>
+                                </tbody>
+                                </table>
+                              </div>
+
+                              </label>
+                            </div>
+
+                            <div class="btn">
+                              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                              <label class="form-check-label" for="gridRadios1">
+                                <div class="card">
+                                <img src="images/fedro43.jpg" class="card-img-top" alt="...">
+                                <table class="table table-striped">
+
+                                <tbody>
+                                  <tr>
+                                    <div class="subtitulocards">
+                                      <h5>FEDRO 3.2W </h5>
+                                    </div>
+                                  <tr>
+                                    <th scope="row">Potencia</th>
+                                    <td>Mark</td>
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Dimensión</th>
+                                    <td>Jacob</td>
+
+                                  </tr>
+                                  <tr>
+                                    <th scope="row">Flujo </th>
+                                    <td>Larry</td>
+                                  </tr>
+                                </tbody>
+                                </table>
+                              </div>
+
+                              </label>
+                            </div>
+
+                          </div>
+
+
+                          <div class="form-group row">
+                              <button class="btn btn-famarilloblanco botonform" type="submit" name="submit2">Siguiente paso</button>
+                        </form>
+</div>
+
+
+
+
+              </main>
+<?php require_once("php/mod/footer.php") ?>
+      </div>
+  </body>
+</html>
