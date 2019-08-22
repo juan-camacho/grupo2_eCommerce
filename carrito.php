4c9eea05e20d3240c5078beb51bf12c7a88e9e04
unknown <fagustinasanchez@gmail.com>
2019-08-22 07:33:31 -0300

    Carrito

---
+++ carrito.php
@@ -0,0 +1,65 @@
+<?php $tittle = "Tienda Babuino - productos"?>
+<!DOCTYPE html>
+<html lang="es">
+<?php require_once("php/mod/head.php")?>
+  <body>
+    <?php require_once("php/mod/header.php"); ?>
+        <main>
+                      <!-- FULLSLIDER -->
+                <!-- FILTROS -->
+          <div class="container" id="containerproducto">
+            <div class="row">
+                <div class="col-lg-8" id="prueba">
+                    <div class="cart-item">
+                        <div class="row align-items-center">
+                            <div class="col-4 lg-2">
+                                hola
+                            </div>
+                            <div class="col-4 lg-2">
+                                hola
+                            </div>
+                            <div class="col-4 lg-2">
+                                hola
+                            </div>
+                            <div class="col-4 lg-2">
+                                hola
+                            </div>
+                        </div>
+
+
+                    </div>
+                    </div>
+
+                <div class="col-lg-4" id="prueba2">
+                    <div class="card">
+                        <div class="card-header">
+                            hola
+                        </div>
+                        <div class="card-body">
+                            hola
+                        </div>
+                        <div class="card-button">
+                            hola
+                        </div>
+                    </div>
+                    <button type="button" name="button"></button>
+
+                    </div>
+
+                </div>
+
+
+               </div>
+            </div>
+
+
+
+
+
+
+              </main>
+
+
+
+  </body>
+</html>
