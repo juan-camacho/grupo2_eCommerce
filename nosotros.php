9813b71311c7c93f5707fe63fc962535f9eb1955
juan <programascamacho@gmail.com>
2019-08-31 20:56:15 -0300

    cambio a partials en todo2

--- nosotros.php
+++ nosotros.php
@@ -1,9 +1,9 @@
 <?php $tittle = "Tienda Babuino - Home"?>
 <!DOCTYPE html>
 <html lang="es">
-<?php require_once("php/mod/head.php")?>
+<?php require_once("partials/head.php")?>
 <body>
-<?php require_once("php/mod/header.php")?>
+<?php require_once("partials/header.php")?>
 <main>
                   <!-- FULLSLIDER -->
                   <div class="fullslider" id="fsnosotros">
@@ -14,6 +14,6 @@
                   </div>
 </main>
 <!-- Footer -->
-<?php require_once("php/mod/footer.php") ?>
+<?php require_once("partials/footer.php") ?>
   </body>
 </html>
