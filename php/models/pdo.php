749e34d536ae98d771576e26d79bb2fe86f4a4fd
Ezequiel Cortés <codelando@gmail.com>
2019-08-31 19:42:46 -0300

    Sea agrega archivo de configuración de ejemplo y se ignora el de cada usuario

--- php/models/pdo.php
+++ php/models/pdo.php
@@ -1,11 +1,10 @@
 <?php
-  $dbHost = 'localhost';
-  $dbName = 'ecommerce';
-  $dbUser = 'root';
-  $dbPass = '';
+
+  require_once __DIR__ . '/../config/config.php';
 
   try {
-      $pdo = new PDO("mysql:host={$dbHost};dbname={$dbName}", $dbUser, $dbPass);
+      $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $e) {
       echo "¡Error!: " . $e->getMessage() . "<br/>";
       die();
