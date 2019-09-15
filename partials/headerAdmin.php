8d1b3abb3b0500e9a2afb542d88b0c127356694b
Juan <programascamacho@gmail.com>
2019-09-15 12:35:32 -0300

    agregado partial

---
+++ partials/headerAdmin.php
@@ -0,0 +1,58 @@
+<nav class="col-12 col-md-3 col-xl-2 bg-dark mt-3 vh-100 pt-5 navAdmin px-0">
+  <h4 class="letrablanca p-3"> Hola Admin! </h4>
+    <div id="accordion">
+  <div class="card letrablanca">
+    <div class="card-header" id="headingOne">
+        <a class="d-flex row mb-0" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <div class="col-2">
+              <i class="fas fa-edit letrablanca"></i>
+          </div>
+          <div class="col-10 pl-0">
+            Páginas
+          </div>
+        </a>
+    </div>
+
+    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        <ul>
+          <li class="py-1 pl-3"> Home </li>
+          <li  class="py-1 pl-3"> Nosotros </li>
+          <li  class="py-1 pl-3"> Contacto </li>
+          <li  class="py-1 pl-3"> FAQ'S </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="card letrablanca">
+    <div class="card-header" id="headingOne">
+        <a class="d-flex row mb-0" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          <div class="col-2">
+              <i class="fas fa-bars letrablanca"></i>
+          </div>
+          <div class="col-10 pl-0">
+            Productos
+          </div>
+        </a>
+    </div>
+
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        <ul>
+          <li class="py-1 pl-3"> Home </li>
+          <li  class="py-1 pl-3"> Nosotros </li>
+          <li  class="py-1 pl-3"> Contacto </li>
+          <li  class="py-1 pl-3"> FAQ'S </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  </div>
+
+
+
+
+</nav>
